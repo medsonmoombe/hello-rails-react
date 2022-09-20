4c49a4253183931ac4efd9285ab138da4de7f4d1
medsonmoombe <medsonmoombe21@gmail.com>
2022-09-20 17:50:23 +0200

    Add the components

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,8 @@
 // Entry point for the build script in your package.json
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
 
 ReactDOM.render(
   <App/>,
